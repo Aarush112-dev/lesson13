d3a5d050a88d76839a97efe99248a6a24e55878d
Aarush112-dev <aarushmenon2020@gmail.com>
2025-02-15 11:41:44 +0000

    Add files via upload

---
+++ main.py
@@ -0,0 +1,48 @@
+class Graph:
+    def __init__(self,num_nodes):
+        self.graph = {}
+        self.sides = num_nodes
+        for i in range(1,self.sides+1):
+            self.graph[i] = []
+    
+    def add_edge(self,x,y):
+        self.graph[x].append(y)
+
+    def iscyclic(self,n,visited,rstack):
+        visited[n] = True
+        rstack[n] = True
+        for node in self.graph[n]:
+            print(node)
+            if visited[node] == False:
+                if self.iscyclic(node,visited,rstack):
+                    return True
+            elif rstack[node] == True:
+                return True
+        rstack[n] = False
+        return False
+    
+    def checkcyclic(self):
+        visited = [False] * self.sides
+        rstack = [False] * self.sides
+        for i in range(1,self.sides+1):
+            if visited[i] == False:
+                if self.iscyclic(i,visited,rstack):
+                    return True
+        return False
+
+        
+
+graph1 = Graph(7)
+graph1.add_edge(1,2)
+graph1.add_edge(2,3)
+graph1.add_edge(3,5)
+graph1.add_edge(5,6)
+graph1.add_edge(6,7)
+graph1.add_edge(7,3)
+print(graph1.graph)
+if graph1.checkcyclic() == True:
+    print("There is a cycle")
+else:
+    print("There isn't a cycle")
+
+        
